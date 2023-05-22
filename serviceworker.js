759566b5523cefc44c2977c4f8b0cdaa92c89ae5
Olivia <15709895+oliviachang29@users.noreply.github.com>
2023-05-22 15:06:17 -0700

    add mission and vision page etc

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2023-04-11T17:36:38.317'
+const CACHE_NAME = 'cache-2023-05-22T22:05:11.367'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
